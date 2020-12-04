94df0ff2e47e1c635dafedb83dfecf67f960b9c0
Brian Murphy <blpmurphy@gmail.com>
2020-12-04 15:28:20 +0000

    Add user recipes cards, Add users favourite recipe cards

--- app.py
+++ app.py
@@ -102,8 +102,10 @@ def my_recipes(username):
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
+    recipes = list(mongo.db.recipes.find())
+
     if session["user"]:
-        return render_template("my_recipes.html", username=username)
+        return render_template("my_recipes.html", username=username, recipes=recipes)
 
     return redirect(url_for("login"))
 
