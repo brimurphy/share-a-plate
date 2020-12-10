37cf40c73b6f15505f2c0246d60f25e98918b59a
Brian Murphy <blpmurphy@gmail.com>
2020-12-10 15:11:43 +0000

    Add back button functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('select').formSelect();
+    // previous page
+    $("#btn-back").click(function() {
+        window.history.back();
+    })
   });
