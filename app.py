d044b88e4929e970c244cdee3274d1d6288b556c
Brian Murphy <blpmurphy@gmail.com>
2020-12-16 11:59:28 +0000

    Update debug=true to debug=false

--- app.py
+++ app.py
@@ -308,4 +308,4 @@ def not_found(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
